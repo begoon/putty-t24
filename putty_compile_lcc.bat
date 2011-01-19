37a0862735ee6a2f511443425c6394b38d3f1f4b
devnull@localhost <devnull@localhost>
2011-01-19 13:27:18 +0000

    Removed LIB and INCLUDE variables from the build script.

--- putty_compile_lcc.bat
+++ putty_compile_lcc.bat
@@ -1,8 +1,5 @@
-set LCC=c:\lcc
+set PATH=c:\lcc\bin;%PATH%
 cd source\windows
-set PATH=%LCC%\bin;%PATH%
-set INCLUDE=%LCC%\include;%INCLUDE%
-set LIB=%LCC%\lib;%LIB
 make -f Makefile.lcc COMPAT="-DNO_MULTIMON -DNO_IPV6" VER="-DRELEASE=0.60.T24.02" putty.exe
 copy putty.exe ..\..
 
