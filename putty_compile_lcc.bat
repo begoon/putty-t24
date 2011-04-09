30f96f3a936cc51ebbeb996fb27b654ffdf3d1a8
alexander <alexander>
2011-04-09 23:07:03 +0000

    Version 0.60.T24.07.1.

--- putty_compile_lcc.bat
+++ putty_compile_lcc.bat
@@ -1,5 +1,5 @@
 set PATH=c:\lcc\bin;%PATH%
 cd source\windows
-make -f Makefile.lcc COMPAT="-DNO_MULTIMON -DNO_IPV6" VER="-DRELEASE=0.60.T24.07.0" putty.exe
+make -f Makefile.lcc COMPAT="-DNO_MULTIMON -DNO_IPV6" VER="-DRELEASE=0.60.T24.07.1" putty.exe
 copy putty.exe ..\..\putty_t24.exe
 
